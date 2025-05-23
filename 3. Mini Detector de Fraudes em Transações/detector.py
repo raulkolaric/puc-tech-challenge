5b69f01de62a8c0f6deeedd82d124561ae847a88
Raul Kolaric <raul.kolaric@gmail.com>
2025-05-23 20:08:47 -0300

    refactor - 3: Folder Structure

--- 3. Mini Detector de Fraudes em Transações/detector.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
